a06d762bc9ffb207c9ee8180794606eaa3b9eb35
Caius Citiriga <nbkmaxine@gmail.com>
2023-10-23 17:51:57 +0200

    fix: exporting services providers

--- dist/modules/ivy-nest-strategies-common/ivy-nest-strategies-common.module.js
+++ dist/modules/ivy-nest-strategies-common/ivy-nest-strategies-common.module.js
@@ -10,13 +10,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.IvyNestStrategiesCommonModule = void 0;
 const common_1 = require("@nestjs/common");
 const ivy_script_config_service_1 = require("./services/ivy-script-config/ivy-script-config.service");
+const ivy_operations_manager_service_1 = require("./services/ivy-operations-manager/ivy-operations-manager.service");
+const ivy_sdk_service_1 = require("./services/ivy-sdk/ivy-sdk.service");
+const ivy_strongest_presence_service_1 = require("./services/ivy-strongest-presence/ivy-strongest-presence.service");
 let IvyNestStrategiesCommonModule = IvyNestStrategiesCommonModule_1 = class IvyNestStrategiesCommonModule {
     static register(options) {
         return {
             module: IvyNestStrategiesCommonModule_1,
-            exports: [ivy_script_config_service_1.IvyScriptConfigService],
-            providers: [
+            exports: [
+                ivy_sdk_service_1.IvySDKService,
                 ivy_script_config_service_1.IvyScriptConfigService,
+                ivy_operations_manager_service_1.IvyOperationsManagerService,
+                ivy_strongest_presence_service_1.IvyStrongestPresenceService,
+            ],
+            providers: [
+                ivy_sdk_service_1.IvySDKService,
+                ivy_script_config_service_1.IvyScriptConfigService,
+                ivy_operations_manager_service_1.IvyOperationsManagerService,
+                ivy_strongest_presence_service_1.IvyStrongestPresenceService,
                 {
                     provide: "IVY_SCRIPT_INITIAL_CONFIG",
                     useValue: options.scriptInitialScriptConfig,
--- src/modules/ivy-nest-strategies-common/ivy-nest-strategies-common.module.ts
+++ src/modules/ivy-nest-strategies-common/ivy-nest-strategies-common.module.ts
@@ -1,6 +1,9 @@
 import { DynamicModule, Module } from "@nestjs/common";
 import { IvyScriptConfigService } from "./services/ivy-script-config/ivy-script-config.service";
 import { ISDKConfigOpts } from "@mof-ivy/ivy-node-sdk";
+import { IvyOperationsManagerService } from "./services/ivy-operations-manager/ivy-operations-manager.service";
+import { IvySDKService } from "./services/ivy-sdk/ivy-sdk.service";
+import { IvyStrongestPresenceService } from "./services/ivy-strongest-presence/ivy-strongest-presence.service";
 
 @Module({})
 export class IvyNestStrategiesCommonModule {
@@ -10,9 +13,18 @@ export class IvyNestStrategiesCommonModule {
   }): DynamicModule {
     return {
       module: IvyNestStrategiesCommonModule,
-      exports: [IvyScriptConfigService],
-      providers: [
+      exports: [
+        IvySDKService,
         IvyScriptConfigService,
+        IvyOperationsManagerService,
+        IvyStrongestPresenceService,
+      ],
+      providers: [
+        IvySDKService,
+        IvyScriptConfigService,
+        IvyOperationsManagerService,
+        IvyStrongestPresenceService,
+
         {
           provide: "IVY_SCRIPT_INITIAL_CONFIG",
           useValue: options.scriptInitialScriptConfig,
